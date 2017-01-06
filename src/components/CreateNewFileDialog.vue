330c4c609399a00f22a5b36978787ca3e8972f16
Tiago Alves <tralves@gmail.com>
2017-01-06 01:16:52 -0200

    Completed CreateNewFileDialog test.

--- src/components/CreateNewFileDialog.vue
+++ src/components/CreateNewFileDialog.vue
@@ -3,14 +3,13 @@
     <mdl-textfield floating-label="File name" :value="filename" id="create-filename-input"></mdl-textfield>
     <template slot="actions">
       <mdl-button primary @click.native="createNew()" id='create-new-file-button'>Create</mdl-button>
-      <mdl-button @click.native="openFromGDrive()">... or open from Google Drive <img v-bind:src="gdriveSvg" /></mdl-button>
+      <mdl-button @click.native="openFromGDrive()" id='open-from-gdrive-button'>... or open from Google Drive <img v-bind:src="gdriveSvg" /></mdl-button>
     </template>
   </mdl-dialog>
 </template>
 
 <script>
-/* global document */
-import { createNewFile, loadFile } from '../vuex/actions'
+import { mapActions } from 'vuex'
 import gdriveSvg from '../assets/google-drive.svg'
 import GapiIntegration from '../gapi/gapi-integration'
 
@@ -22,24 +21,27 @@ export default {
     }
   },
   methods: {
+    ...mapActions([
+      'createNewFile',
+      'loadFile'
+    ]),
     openDialog () {
       this.filename = 'New document'
       this.$refs.create_new_file.open()
     },
     createNew () {
       this.$refs.create_new_file.close()
-      createNewFile(this.$el.querySelector('#create-filename-input').value)
+      this.createNewFile(this.$el.querySelector('#create-filename-input').value)
     },
     openFromGDrive () {
       this.$refs.create_new_file.close()
       GapiIntegration.showPicker()
         .then((id) => {
-          console.log('open file:' + id)
+          console.log('open file: ' + id)
           if (id !== this.fileId) {
             GapiIntegration.loadFile(id)
             .then(file => {
-              console.log('GOT FILE!!!!!!')
-              loadFile(file)
+              this.loadFile(file)
             })
           }
         })
