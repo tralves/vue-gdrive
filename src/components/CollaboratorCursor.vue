d8c76f06dd27a4a4f68d6d98fb1261cdcb2dd033
Tiago Alves <tralves@gmail.com>
2017-02-08 02:03:24 -0200

    Added missing files.

---
+++ src/components/CollaboratorCursor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :style="{top: cursor.position.top + 'px', left: cursor.position.left +'px'}">
+    <mdl-tooltip :target="'carret'+cursor.sessionId">
+      {{ cursor.collaboratorName }}
+    </mdl-tooltip>
+    <div class="carret" :id="'carret'+cursor.sessionId" :style="{backgroundColor: cursor.color}">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['cursor']
+  }
+</script>
+
+<style scoped>
+  .carret {
+    position: absolute;
+    width: 2px;
+    height: 20px;
+    margin-top: -2px;
+  }
+</style>
